a7036dffd345ab096269cef4874ea4f83728c727
Henrik Brinch van Meenen <henrikbvm2@gmail.com>
2023-10-24 17:14:09 +0200

    Outline created for backend pipeline

--- src/backend_pipeline.py
+++ src/backend_pipeline.py
@@ -2,3 +2,13 @@
 This file contains the code to run the backend pipeline without the GUI.
 It will be used for testing the project to ensure backend logic works as expected.
 """
+
+
+# Step 1. Load the images, verify that they are valid images (dimensions > 0 and 3 channels. Also dim(A) == dim(B))))
+
+# Step 2. Detect the facial landmarks for both images.
+
+# Step 3. Interpolate the facial landmarks using IDW.
+
+# Step 4. Project the interpolated facial landmarks to the GAN.
+
