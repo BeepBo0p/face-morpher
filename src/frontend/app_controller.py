dcf11c8550839cb7992f96ff10f19d0cd1223a78
BeepBoop <henrikbvm2@gmail.com>
2023-11-01 14:51:02 +0100

    Created skeleton controller

--- src/frontend/app_controller.py
+++ src/frontend/app_controller.py
@@ -2,3 +2,39 @@
 File containing the functions used to control the application. Should do basic logic and call functions from the backend.
 """
 import backend as bk
+
+def open_image():
+    """
+    Opens an image from the file system and displays it on the canvas.
+    """
+    pass
+
+def add_feature_point():
+    """
+    Adds a feature point to the canvas.
+    """
+    pass
+
+def move_feature_point():
+    """
+    Moves a feature point on the canvas.
+    """
+    pass
+
+def delete_feature_point():
+    """
+    Deletes a feature point from the canvas.
+    """
+    pass
+
+def validate_settings():
+    """
+    Validates the settings and returns a boolean.
+    """
+    pass
+
+def morph_images():
+    """
+    Morphs the images and displays them on the canvas.
+    """
+    pass
